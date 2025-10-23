b2714e506bfca1d6e920a1b405e0203d4c82a3e9
Carolina Silva dos Santos <carolina.sistemax@gmail.com>
2025-10-22 21:07:34 -0300

    add animation files

---
+++ AnimationWorkshop/App/AnimationWorkshopApp.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationWorkshopApp.swift
+//  AnimationWorkshop
+//
+//  Created by Carolina Silva dos Santos on 22/10/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AnimationWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Example()
+        }
+    }
+}
---
+++ AnimationWorkshop/Example.swift
@@ -0,0 +1,18 @@
+//
+//  Example.swift
+//  AnimationWorkshop
+//
+//  Created by Carolina Silva dos Santos on 22/10/25.
+//
+
+import SwiftUI
+
+struct Example: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Example()
+}
